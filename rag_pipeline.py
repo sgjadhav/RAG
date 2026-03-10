10485b9483ac091c05f09510d9cca8be5ab79f89
sgjadhav <sgjadhav17@gmail.com>
2026-03-11 00:57:19 +0530

    Implement RAG chain with Google Gemini LLM

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,1 +1,42 @@
 
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+import os
+
+
+def build_rag_chain(vectorstore):
+
+    # Create retriever
+    retriever = vectorstore.as_retriever(search_kwargs={"k":4})
+
+    # Gemini LLM
+    llm = ChatGoogleGenerativeAI(
+        model="models/gemini-2.5-flash",
+        google_api_key=os.getenv("GOOGLE_API_KEY"),
+        temperature=0
+    )
+
+    # Prompt template
+    prompt = ChatPromptTemplate.from_template(
+        """
+        Answer the question based only on the context below.
+
+        Context:
+        {context}
+
+        Question:
+        {question}
+        """
+    )
+
+    # RAG chain
+    rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    return rag_chain
