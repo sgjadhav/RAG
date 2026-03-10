5d4956025b3bc83d807ee8d594993323cbd13189
sgjadhav <sgjadhav17@gmail.com>
2026-03-11 00:33:08 +0530

    Implement document loading from URLs
    Added a function to load documents from given URLs using UnstructuredURLLoader.

--- document_loader.py
+++ document_loader.py
@@ -1,1 +1,13 @@
 
+from langchain_community.document_loaders import UnstructuredURLLoader
+
+def load_documents(urls):
+    """
+    Load documents from URLs
+    """
+
+    loader = UnstructuredURLLoader(urls=urls)
+
+    documents = loader.load()
+
+    return documents
